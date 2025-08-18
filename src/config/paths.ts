2e4d51cbe89ee61173169eb521014a6219a883cc
Italo <italo.saldanha@vonbraunlabs.com.br>
2025-08-18 17:00:13 -0300

    update docker compose

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,9 +1,11 @@
 import path from "path";
-const BASE_PROJECT_PATH = "/Users/italomatos/Documents/IC";
+const ROOT_UDLF_PATH = process.env.HOST_UDLF_PATH || "";
+const APP_DATASETS_PATH = process.env.APP_DATASETS_PATH || "/app/datasets";
+const lists_mpeg7_path = path.join(APP_DATASETS_PATH, "mpeg7/lists_mpeg7.txt");
 
 export const paths = {
   uploads: path.join(process.cwd(), "uploads"),
-  outputs: path.join(BASE_PROJECT_PATH, "udlf-api/outputs"),
-  datasetList: path.join(BASE_PROJECT_PATH, "UDLF/Datasets/mpeg7/lists_mpeg7.txt"),
-  datasetImages: path.join(BASE_PROJECT_PATH, "UDLF/Datasets/mpeg7/original/"),
+  outputs: path.join(ROOT_UDLF_PATH, "udlf-api/outputs"),
+  datasetList: lists_mpeg7_path,
+  datasetImages: path.join(APP_DATASETS_PATH, "mpeg7/original"),
 };
